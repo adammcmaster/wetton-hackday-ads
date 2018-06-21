5d6ab0c9e1b17cdbf115ecbd73d81e5e9d92ccee
Adam McMaster <adam@zooniverse.org>
2018-06-21 14:40:58 +0100

    Fetching/caching authors

--- hack.py
+++ hack.py
@@ -1,47 +1,90 @@
 import ads as ads
+import progressbar
+
+import json
 
 with open('api.conf') as api_f:
     ads.config.token = api_f.read().strip()
 
 SEARCH_DEPTH = 3
-STARTING_QUERY = 'galaxy zoo'
+STARTING_QUERY = 'Lintott, C'
+
+CACHED_PAPERS = {}
+REMAINING_API_CALLS = 0
+
+#def extend_authors(authors, seen_papers, papers):
+#    new_authors = set()
+#    for paper, paper_authors in papers.values():
+#        if paper in seen_papers:
+#            continue
+#        seen_papers.add(paper)
+#        for author in paper_authors:
+#            if author not in authors:
+#                new_authors.add(author)
+#                authors[author] = set()
+#            coauthors = authors[author]
+#            for coauthor in paper.author:
+#                if author == coauthor:
+#                    continue
+#                coauthors.add(coauthor)
+#    return new_authors
 
 def extend_authors(authors, seen_papers, papers):
-    new_authors = set()
-    for paper in papers:
-        if paper.id in seen_papers:
+    for paper, paper_authors in papers.items():
+        if paper in seen_papers:
             continue
-        seen_papers.add(paper.id)
-        if not paper.author:
-            continue
-        for author in paper.author:
-            if author not in authors:
-                new_authors.add(author)
-                authors[author] = set()
-            coauthors = authors[author]
-            for coauthor in paper.author:
-                if author == coauthor:
-                    continue
-                coauthors.add(coauthor)
-    return new_authors
+        seen_papers.add(paper)
+        for author in paper_authors:
+            authors[author] = (
+                authors.get(author, set()) | set(paper_authors) - set(author)
+            )
 
-authors = {}
-seen_papers = set()
+def cached_query(q):
+    global CACHED_PAPERS
+    global REMAINING_API_CALLS
 
-starting_papers = ads.SearchQuery(q=STARTING_QUERY)
-extend_authors(authors, seen_papers, starting_papers)
-new_authors = authors.keys()
-print(authors)
+    if not CACHED_PAPERS:
+        with open('/opt/cache/cache.json') as cache_f:
+            CACHED_PAPERS = json.load(cache_f)
 
-for i in range(SEARCH_DEPTH - 1):
-    next_authors = set()
-    for author in new_authors:
-        next_authors = (
-            next_authors
-            | extend_authors(authors, seen_papers,
-                             ads.SearchQuery(author=author)
-                            )
-        )
-    new_authors = next_authors
-    print("New authors: {}".format(new_authors))
-    print(authors)
+    if q not in CACHED_PAPERS:
+        results = ads.SearchQuery(author=q)
+        CACHED_PAPERS[q] = {}
+        for paper in results:
+            if not paper.author:
+                continue
+            CACHED_PAPERS[q][paper.bibcode] = paper.author
+
+        REMAINING_API_CALLS = results.response.get_ratelimits()['remaining']
+
+    return CACHED_PAPERS[q]
+
+try:
+    authors = {}
+    seen_papers = set()
+
+    print("Getting initial authors")
+
+    starting_papers = cached_query(q=STARTING_QUERY)
+    extend_authors(authors, seen_papers, starting_papers)
+    new_authors = set(authors.keys())
+    previous_authors = new_authors
+
+    for i in range(1, SEARCH_DEPTH - 1):
+        print("Getting coauthors (iteration {})".format(i))
+        for author in progressbar.progressbar(new_authors, redirect_stdout=True):
+            q = cached_query(q=author)
+            extend_authors(authors, seen_papers, q)
+
+        print("Remaining ADS API calls: {}".format(REMAINING_API_CALLS))
+
+        with open('/opt/cache/cache.json', 'w') as cache_f:
+            json.dump(CACHED_PAPERS, cache_f)
+
+        all_authors = set(authors.keys())
+        new_authors = all_authors - previous_authors
+        previous_authors = all_authors
+finally:
+    print("Remaining ADS API calls: {}".format(REMAINING_API_CALLS))
+    with open('/opt/cache/cache.json', 'w') as cache_f:
+        json.dump(CACHED_PAPERS, cache_f)
